9d1cf678382df5eddf21c5805cc7a982bdaf6209
Alfredo Lopez <alfredo.lopez@soprasteria.com>
2025-11-15 07:12:11 +0100

    Add connection to Redis and dependency

--- src/main/java/com/jember/alfredo/prueba/PruebaApplication.java
+++ src/main/java/com/jember/alfredo/prueba/PruebaApplication.java
@@ -2,8 +2,10 @@ package com.jember.alfredo.prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PruebaApplication {
 
 	public static void main(String[] args) {
